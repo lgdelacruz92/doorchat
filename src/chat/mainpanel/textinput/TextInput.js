0c71c1e1f97ff776955c4b139cbaee920d442abc
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-02-27 06:26:42 -0800

    Text field workds

--- src/chat/mainpanel/textinput/TextInput.js
+++ src/chat/mainpanel/textinput/TextInput.js
@@ -1,15 +1,42 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
         minHeight: '80px',
-        borderTop: '1px solid lightgrey'
+        borderTop: '1px solid lightgrey',
+        padding: '20px',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    textField: {
+        width: '90%',
+        display: 'inline-block',
+        overflow: 'hidden',
+        minHeight: '40px',
+        lineHeight: '20px',
+        padding: '10px'
+    },
+    send: {
+        width: '100px',
+        display: 'inline-block',
+        marginLeft: '10px',
+        color: '#4ec0ee',
+        fontWeight: 'bold'
     }
 })
 
 const TextInput = () => {
     const classes = useStyles();
-    return <div className={classes.root}>Hello world</div>;
+    return <div className={classes.root}>
+        <span
+            className={classes.textField}
+            role="textbox"
+            contentEditable={true}>
+            Hello world
+        </span>
+        <Button className={classes.send}>Send</Button>
+    </div>;
 }
 
 export default TextInput;
